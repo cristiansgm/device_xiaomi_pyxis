8396876f032207c7ffd4e90f1ba18d7b5059f723
cristiansgm <cristiansaezgm@gmail.com>
2022-03-05 19:00:53 +0100

    pyxis: initial ArrowOS bringup

--- arrow_pyxis.mk
+++ arrow_pyxis.mk
@@ -4,13 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from pyxis device
 $(call inherit-product, device/xiaomi/pyxis/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_pyxis
+PRODUCT_NAME := arrow_pyxis
 PRODUCT_DEVICE := pyxis
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
